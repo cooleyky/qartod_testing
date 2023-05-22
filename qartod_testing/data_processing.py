d709eaed7255f96fe022b4cf61f52fff92c7864a
Kylene Cooley <kylene.cooley@whoi.edu>
2023-05-22 15:15:35 -0400

    Find different flags and save counts of  QARTOD flags

--- qartod_testing/data_processing.py
+++ qartod_testing/data_processing.py
@@ -1,17 +1,17 @@
 import os
 
-def build_data_path(refdes,method,stream,type,folder='interim'):
+def build_data_path(refdes,method,stream,prefix,folder='interim',suffix='.nc'):
     # Input: 
     #   refdes: string built from OOI site, node, and sensor for chosen dataset
     #   method: 'recovered_inst', 'recovered_host', or 'telemetered'(?) 
     #   stream: name of data stream 
-    #   type: 'prod' or 'dev'
+    #   source: 'prod' or 'dev'
     #   folder: 'interim' (default), 'processed', 'raw', or 'external'
     #
     # Returns:
     #   ds_path: relative path to dataset from notebook folder
     
-    filename = '-'.join((type,refdes,method,stream))+'.nc'              # build filename from dataset type and source
+    filename = '-'.join((prefix,refdes,method,stream))+suffix              # build filename from dataset type and source
 
     data_folder = os.path.relpath('../data')                            # path to data folder from notebook folder
 
