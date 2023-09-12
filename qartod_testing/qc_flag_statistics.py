074930b3b2213ce2d7724983a5f901497bde7719
Kylene Cooley <kylene.cooley@whoi.edu>
2023-09-12 19:30:54 +0000

    Update names of project libraries in notebooks

--- qartod_testing/qc_flag_statistics.py
+++ qartod_testing/qc_flag_statistics.py
@@ -272,8 +272,7 @@ def dev1_data_request(site, node, sensor, method, stream, params):
                         if time_pattern.match(ds[v].attrs['units']):
                             del(ds[v].attrs['_FillValue'])  # no fill values for time!
                             ds[v].attrs['units'] = 'seconds since 1900-01-01T00:00:00.000Z'
-                            np_time = ntp_date 
-                                    + (ds[v] * 1e9).astype('timedelta64[ns]')
+                            np_time = ntp_date + (ds[v] * 1e9).astype('timedelta64[ns]')
                             ds[v] = np_time
 
             # Sort by time
