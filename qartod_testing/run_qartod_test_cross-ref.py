1275f3e33950dc819f4175ff845fa0cb0021a3c0
Kylene Cooley <kylene.cooley@whoi.edu>
2025-01-15 22:09:56 +0000

    Add method for importing updated data via M2M

--- qartod_testing/run_qartod_test_cross-ref.py
+++ qartod_testing/run_qartod_test_cross-ref.py
@@ -12,11 +12,8 @@ import ast
 from glob import glob
 
 # Import functions from ooinet and ooi_data_explorations libraries
-from ooi_data_explorations.common import load_kdata, get_vocabulary
-from ooinet import M2M
-
-# Import functions from ooinet and ooi_data_explorations libraries
-from ooi_data_explorations.common import load_kdata, get_vocabulary
+from ooi_data_explorations.common import load_kdata, get_vocabulary, \
+    m2m_request, m2m_collect
 from ooinet import M2M
 
 # Import functions from project qc_completion module
@@ -25,14 +22,15 @@ from qartod_testing.qc_completion import load_gross_range_qartod_test_list, \
     check_tests_exe, make_results_table, add_test_exe, write_results
 
 # Define site for refdes search and find datasets available
-site = 'CP10CNSM'
+site = 'CP02PMCI'
+prefix = 'CP0'
 datasets = M2M.search_datasets(site)
 datasets.reset_index(inplace=True)
 datasets.drop(labels="index", axis=1, inplace=True)
 
 # Set csv save directory and file name for results
 csv_name = f"{site}_test_cross-ref_results.csv"
-csv_dir = "./data/processed/"
+csv_dir = f"./data/processed/{prefix}_tests_completed2/"
 # loop through sensors to check and find datastreams available
 for k in datasets.index:
     refdes = datasets.refdes[k]
@@ -52,15 +50,37 @@ for k in datasets.index:
         else:
             # Load data
             get_vocabulary(site, node, sensor)
+            print(f"Loading deployment {deploy}")
             data = load_kdata(site, node, sensor, method, stream, ('*deployment%04d*%s*.nc' % (deploy, instclass)))
+            while data is None:
+                deploy+=1
+                if deploy > datasets.deployments[k][-1]:
+                    print(f"No dataset available for {refdes}-{stream} tests.")
+                    break
+                else:
+                    print(f"Loading deployment {deploy}")
+                    data = load_kdata(site, node, sensor, method, stream, ('*deployment%04d*%s*.nc' % (deploy, instclass)))
             try:
                 print(data.id) # to check data stream loaded
             except AttributeError:
-                print(f"No dataset available for {refdes}-{stream} tests.")
                 del [grt_table, ct_table]
             else:
                 # Create a dictionary of key-value pairs of dataset variable name:alternate parameter name
                 test_parameters = make_test_parameter_dict(data)
+                # If QARTOD test not executed on kdata load data with M2M request
+                if len(test_parameters)==0:
+                    deploy_info = M2M.get_deployments(refdes, deploy_num=str(deploy))
+                    dt_start = deploy_info.deployStart[0]
+                    dt_end = deploy_info.deployStart[0] + pd.Timedelta(5, 'D')
+                    dt_start = dt_start.isoformat(timespec='milliseconds')+'Z'
+                    dt_end = dt_end.isoformat(timespec='milliseconds')+'Z'
+                    m2m_result = m2m_request(site, node, sensor, method, stream,
+                         start=dt_start, stop=dt_end)
+                    try:
+                        data = m2m_collect(m2m_result)
+                        test_parameters = make_test_parameter_dict(data)
+                    except:
+                        print("M2M data request or collection failed")
                 # Use test parameters to check for tests executed in dataset
                 test_exe = check_tests_exe(data, test_parameters, grt_table, ct_table)
                 # Make table for cross-ref results
